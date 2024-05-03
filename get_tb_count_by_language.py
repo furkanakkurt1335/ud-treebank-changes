79a855cfe9397c86e552499d2e12a6341285126e
Furkan Akkurt <furkanakkurt9285@gmail.com>
2024-05-03 13:53:24 +0300

    update repo count with latest version

--- get_tb_count_by_language.py
+++ get_tb_count_by_language.py
@@ -10,7 +10,7 @@ def main(args):
     repos_path = Path(args.repos)
     with repos_path.open() as f:
         repos = json.load(f)
-    
+
     lang_tb_pattern = 'UD_(.+)-(.+)'
 
     lang_d = {}
@@ -18,27 +18,30 @@ def main(args):
         match = re.match(lang_tb_pattern, repo)
         if match:
             lang = match.group(1)
+            tags = repos[repo]['tags']
+            if not tags or tags[-1] != 'r2.13':
+                continue
             if lang not in lang_d:
                 lang_d[lang] = []
             lang_d[lang].append(repo)
-    
+
     lang_tb_count_path  = repos_path.parent / 'lang_tb_count.json'
     with lang_tb_count_path.open('w') as f:
         json.dump(lang_d, f, indent=4)
-    
+
     count_d = {}
     for lang, repos in lang_d.items():
         tb_count  = len(repos)
         if tb_count not in count_d:
             count_d[tb_count] = 0
         count_d[tb_count] += 1
-    
+
     count_path  = repos_path.parent / 'tb_count.json'
     with count_path.open('w') as f:
         json.dump(count_d, f, indent=4, sort_keys=True)
-    
+
     print('Done')
-    
+
 if __name__ == '__main__':
     args = get_args()
     main(args)
